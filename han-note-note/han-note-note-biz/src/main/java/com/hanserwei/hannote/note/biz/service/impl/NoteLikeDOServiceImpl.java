e17ab857b9eeced9371c2a3ebd77bb5204529d0c
Hanserwei <2628273921@qq.com>
2025-10-15 19:33:21 +0800

    refactor(note):优化NoteLikeDOServiceImpl的导入顺序- 调整了类导入顺序，将Spring注解与MyBatis相关依赖分开 - 移除了未使用的List导入 -重新组织了包导入顺序以提高可读性 -保持了@Service注解的位置不变- 确保所有必需的依赖仍然正确导入

--- han-note-note/han-note-note-biz/src/main/java/com/hanserwei/hannote/note/biz/service/impl/NoteLikeDOServiceImpl.java
+++ han-note-note/han-note-note-biz/src/main/java/com/hanserwei/hannote/note/biz/service/impl/NoteLikeDOServiceImpl.java
@@ -1,12 +1,10 @@
 package com.hanserwei.hannote.note.biz.service.impl;
 
-import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Autowired;
-import java.util.List;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.hanserwei.hannote.note.biz.domain.mapper.NoteLikeDOMapper;
 import com.hanserwei.hannote.note.biz.domain.dataobject.NoteLikeDO;
+import com.hanserwei.hannote.note.biz.domain.mapper.NoteLikeDOMapper;
 import com.hanserwei.hannote.note.biz.service.NoteLikeDOService;
+import org.springframework.stereotype.Service;
 @Service
 public class NoteLikeDOServiceImpl extends ServiceImpl<NoteLikeDOMapper, NoteLikeDO> implements NoteLikeDOService{
 
