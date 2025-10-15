893f52e5aa3e83ae3cb1ee8016f7c722d51ff570
Hanserwei <2628273921@qq.com>
2025-10-15 19:10:48 +0800

    feat(count): 初始化计数服务模块
    - 新增 han-note-count 模块及其子模块 han-note-count-api 和 han-note-count-biz
    - 配置 application.yml 和 bootstrap.yml，支持 Nacos 注册与配置中心
    - 添加 MyBatis-Plus、Redis、MySQL 等基础依赖
    - 集成日志配置 logback-spring.xml，支持异步输出及多环境配置
    - 设置 .gitignore 忽略本地开发配置文件
    - 更新 IDEA 编码配置，指定新增模块的字符集为 UTF-8- 在主 pom.xml 中注册新模块 han-note-count

---
+++ han-note-count/han-note-count-biz/src/main/java/com/hanserwei/hannote/count/biz/domain/HannoteCountBizApplication.java
@@ -0,0 +1,13 @@
+package com.hanserwei.hannote.count.biz.domain;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.hanserwei.hannote.count.biz.domain.mapper")
+public class HannoteCountBizApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HannoteCountBizApplication.class, args);
+    }
+}
